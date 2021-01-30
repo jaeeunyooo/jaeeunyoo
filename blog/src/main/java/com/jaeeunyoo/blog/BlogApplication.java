5f03b1e33e670cf075007694e8cfdd2e977b271f
jaeeaunyoo <frontiersofme@gmail.com>
2021-01-31 04:03:13 +0900

    pid 기록하도록 수정

--- blog/src/main/java/com/jaeeunyoo/blog/BlogApplication.java
+++ blog/src/main/java/com/jaeeunyoo/blog/BlogApplication.java
@@ -2,6 +2,7 @@ package com.jaeeunyoo.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
@@ -12,6 +13,7 @@ public class BlogApplication {
 
     public static void main(String[] args) {
         SpringApplication springApplication = new SpringApplication(BlogApplication.class);
+        springApplication.addListeners(new ApplicationPidFileWriter());
         springApplication.run(args);
     }
 }
