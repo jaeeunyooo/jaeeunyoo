cf18a961f4bbd1c82b0674c2c7210d38b2e6e624
jaeeaunyoo <frontiersofme@gmail.com>
2021-02-03 01:00:14 +0900

    add trendingTags on About page

--- blog/src/main/java/com/jaeeunyoo/blog/domain/about/AboutController.java
+++ blog/src/main/java/com/jaeeunyoo/blog/domain/about/AboutController.java
@@ -1,13 +1,20 @@
 package com.jaeeunyoo.blog.domain.about;
 
+import com.jaeeunyoo.blog.domain.tag.application.TagService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class AboutController {
 
+    private final TagService tagService;
+
     @GetMapping("/about")
-    public String about() {
+    public String about(Model model) {
+        model.addAttribute("trendingTags", tagService.getTrendingTags());
         return "layer/about";
     }
 }
